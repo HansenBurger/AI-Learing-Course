4a1d999389992c53e88f96af9d148de89416c5c8
HansenBurger <1035376945@qq.com>
2022-10-02 18:13:18 +0800

    few changes 1. display of calculation 2. change the calculation of coordinate map between base_pad and conv

--- Course_03_Digtal_image/3_graph_convolution/01_convolution.py
+++ Course_03_Digtal_image/3_graph_convolution/01_convolution.py
@@ -48,7 +48,7 @@ class Basic:
 
 class Convolution(Basic):
     '''
-    convolution for matrix (single & multiple)
+    convolution for matrix (single) -same mode
     :param: p_st: padding setting
     :param: s_st: stride setting
     '''
@@ -103,8 +103,8 @@ class Convolution(Basic):
         conv_out = self.__EmptyMatrix(base, f)
         for i in range(conv_out.shape[0]):
             for j in range(conv_out.shape[1]):
-                i_ = i + (f - 1) / 2 + self.__s_st - 1  # coord in base pad
-                j_ = j + (f - 1) / 2 + self.__s_st - 1  # coord in base pad
+                i_ = (f - 1) / 2 + self.__s_st * i  # coord in base pad
+                j_ = (f - 1) / 2 + self.__s_st * j  # coord in base pad
                 m_p = base_pad[int(i_ - (f - 1) / 2):int(i_ + (f + 1) / 2),
                                int(j_ - (f - 1) / 2):int(j_ + (f + 1) / 2)]
                 conv_v = np.sum((m_p * filter_).ravel())
@@ -118,7 +118,7 @@ class ImageFiltering(Basic):
     def __init__(self, img_loc: str) -> None:
         super().__init__()
         self.__img = cv2.imread(img_loc)
-        _, _, self.__c = self.__img.shape  # gray img still got multi channel
+        _, _, self.__c = self.__img.shape  # gray img still got multiple channel
 
     def __ImgFilt(self,
                   filter_: np.ndarray,
@@ -157,18 +157,36 @@ class ImageFiltering(Basic):
         return img_
 
 
-class ConvCalculation(Basic):
+class MatrixCalculation(Basic):
 
     def __init__(self, matrix_in: np.ndarray, p_st: int, s_st: int) -> None:
         super().__init__()
         self.__m_in = matrix_in
-        self.__con_ = Convolution(p_st, s_st)
+        self.__conv = Convolution(p_st, s_st)
 
-    def Calculation(self, w_in: np.ndarray, b_in: np.ndarray):
-        if len(self.__m_in.shape) < 3:
-            return self.__con_.ConvFilt(self.__m_in, w_in) + b_in
+    def Convlution(self, w_in: np.ndarray, b_in: np.ndarray):
+        '''
+        Calculation for convlution of matrixes
+        :param: w_in: kernal (num of channel must be equal)
+        :param: b_in: bias of the kernal
+        :return: conv_out: results of convlution
+        '''
+        if len(self.__m_in.shape) != len(w_in.shape):
+            return
         else:
-            pass
+            if len(self.__m_in.shape) == 2:
+                conv_out = self.__conv.ConvFilt(self.__m_in, w_in) + b_in
+            else:
+                conv_out_s = [
+                    self.__conv.ConvFilt(self.__m_in[:, :, c], w_in[:, :, c])
+                    for c in range(self.__m_in.shape[2])
+                ]
+                conv_out = np.zeros(conv_out_s[0].shape,
+                                    dtype=conv_out_s[0].dtype)
+                for i in conv_out_s:
+                    conv_out += i
+                conv_out += b_in
+        return conv_out
 
 
 def ImgProcessDisplay(img: np.ndarray, ax: any, title: str):
@@ -179,8 +197,10 @@ def ImgProcessDisplay(img: np.ndarray, ax: any, title: str):
 
 def main():
     filt_p = ImageFiltering(img_loc)
-    fig, ((ax_0_0, ax_0_1, ax_0_2), (ax_1_0, ax_1_1,
-                                     ax_1_2)) = plt.subplots(2, 3)
+    fig_0, ((ax_0_0, ax_0_1, ax_0_2), (ax_1_0, ax_1_1,
+                                       ax_1_2)) = plt.subplots(2, 3)
+    fig_0.suptitle("Image Filtering")
+
     ImgProcessDisplay(filt_p.ImgItself(), ax_0_0, 'Origin')
     ImgProcessDisplay(filt_p.ImgSmooth_mean(), ax_0_1, 'Mean')
     ImgProcessDisplay(filt_p.ImgSmooth_gauss(), ax_0_2, 'Gauss')
@@ -188,7 +208,69 @@ def main():
     ImgProcessDisplay(filt_p.ImgSoble_hor(), ax_1_1, 'Soble(h)')
     ImgProcessDisplay(filt_p.ImgSoble_ver(), ax_1_2, 'Soble(v)')
 
-    fig.suptitle("Image Filtering")
+    fig_0.tight_layout()
+
+    fig_1, ax_es = plt.subplots(nrows=4, ncols=1, figsize=(9, 12))
+    fig_1.suptitle('Matrix Convlution (pad=1, stride=2)')
+
+    conv_cal = MatrixCalculation(test_volume, p_st=1, s_st=2)
+    conv_out_0 = conv_cal.Convlution(filter_0_w, filter_0_b)
+    conv_out_1 = conv_cal.Convlution(filter_1_w, filter_1_b)
+
+    for ax in ax_es:
+        ax.remove()
+
+    gridspec = ax_es[0].get_subplotspec().get_gridspec()
+    subfigs = [fig_1.add_subfigure(gs) for gs in gridspec]
+
+    subfigs[0].suptitle('Input volume')
+    ax_row_0 = subfigs[0].subplots(nrows=1, ncols=3)
+
+    for i in range(len(ax_row_0)):
+        sns.heatmap(test_volume[:, :, i],
+                    linewidths=0.2,
+                    annot=True,
+                    cbar=False,
+                    ax=ax_row_0[i],
+                    cmap='rocket')
+
+    subfigs[1].suptitle('Filter w0, b0 = {0}'.format(filter_0_b))
+    ax_row_1 = subfigs[1].subplots(nrows=1, ncols=3)
+
+    for i in range(len(ax_row_1)):
+        sns.heatmap(filter_0_w[:, :, i],
+                    linewidths=0.2,
+                    annot=True,
+                    cbar=False,
+                    ax=ax_row_1[i],
+                    cmap='YlOrBr')
+
+    subfigs[2].suptitle('Filter w1, b1 = {0}'.format(filter_1_b))
+    ax_row_2 = subfigs[2].subplots(nrows=1, ncols=3)
+
+    for i in range(len(ax_row_2)):
+        sns.heatmap(filter_1_w[:, :, i],
+                    linewidths=0.2,
+                    annot=True,
+                    cbar=False,
+                    ax=ax_row_2[i],
+                    cmap='YlOrBr')
+
+    subfigs[3].suptitle('Output Volume')
+    ax_row_3 = subfigs[3].subplots(nrows=1, ncols=2)
+    sns.heatmap(conv_out_0,
+                linewidths=0.2,
+                annot=True,
+                cbar=False,
+                ax=ax_row_3[0],
+                cmap='Blues')
+    sns.heatmap(conv_out_1,
+                linewidths=0.2,
+                annot=True,
+                cbar=False,
+                ax=ax_row_3[1],
+                cmap='Blues')
+
     plt.show()
     plt.close()
 
