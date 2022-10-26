e5515cb62bd12e1745c580cb26185b0724070bcb
HansenBurger <1035376945@qq.com>
2022-10-26 11:47:00 +0800

    HierCluster detail complete

--- Course_08_Image_clustering/2_Hierarchical/01_hier_detail.py
+++ Course_08_Image_clustering/2_Hierarchical/01_hier_detail.py
@@ -1,5 +1,7 @@
 import numpy as np
 from numpy import linalg
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
 
 
 class Basic():
@@ -14,11 +16,8 @@ class HierCluster(Basic):
         super().__init__()
         self.__link_way = self.__Method(method)
         self.__arr2multiD = lambda a: np.array([a]) if len(a.shape) < 2 else a
-        self.__data_st = None
-        self.__data_lb = None
 
     def __Euclidean(self, arr_0: np.ndarray, arr_1: np.ndarray) -> np.ndarray:
-        arr_0, arr_1 = self.__arr2multiD(arr_0), self.__arr2multiD(arr_1)
         dist_result = np.zeros((arr_0.shape[0], arr_1.shape[0]))
         for i in range(arr_0.shape[0]):
             for j in range(arr_1.shape[0]):
@@ -36,52 +35,72 @@ class HierCluster(Basic):
             link_way = lambda a0, a1: self.__Euclidean(a0, a1).mean()
         return link_way
 
-    def fit(self, d: np.ndarray, k: int):
-        total_gp = d.tolist()
-        gp_index = np.linspace(0, len(total_gp), len(total_gp),
-                               endpoint=False).astype(np.int32)
-        loop_rs = []
+    def __DistMatrix(self, arr_: np.ndarray, m_sz: int) -> np.ndarray:
 
-        while (gp_index.shape[0] != 1):
-            gp = [self.__arr2multiD(np.array(total_gp[i])) for i in gp_index]
-            sz = gp_index.shape[0]
-            dist_rs = np.zeros((sz, sz))
+        dist_m = np.zeros((m_sz, m_sz))
 
-            for i in range(sz):
-                for j in range(sz):
-                    if i == j:
-                        dist_rs[i, j] = -1
+        for i in range(m_sz):
+            for j in range(m_sz):
+                if i == j:
+                    dist_m[i, j] = -1
+                else:
+                    v_i, sz_i = arr_[i], arr_[i].shape[0]
+                    v_j, sz_j = arr_[j], arr_[j].shape[0]
+                    if sz_i == 1 or sz_j == 1:
+                        dist_m[i, j] = self.__Method('AverageLink')(v_i, v_j)
                     else:
-                        v_i, sz_i = gp[i], gp[i].shape[0]
-                        v_j, sz_j = gp[j], gp[j].shape[0]
-                        if sz_i == 1 or sz_j == 1:
-                            dist_rs[i, j] = self.__Euclidean(v_i, v_j)[0]
-                        else:
-                            dist_rs[i, j] = self.__link_way(v_i, v_j)
+                        dist_m[i, j] = self.__link_way(v_i, v_j)
+
+        return dist_m
+
+    def fit(self, d: np.ndarray, k: int):
+        total_gp = d.reshape(d.shape[0], np.array(d[0]).size).tolist()
+        gp_ind = np.linspace(0, d.shape[0], d.shape[0],
+                             endpoint=False).astype(np.int32)
+        loop_rs = []
+        gp_inds = [gp_ind]
+
+        while (gp_ind.shape[0] != 1):
+            gp = [np.array(total_gp[i]) for i in gp_ind]
+            sz = gp_ind.shape[0]
+            dist_rs = self.__DistMatrix(gp, sz)
 
             min_dist = dist_rs.ravel()[np.where(dist_rs.ravel() >= 0)].min()
-            ind_0, ind_1 = np.where(dist_rs == min_dist)[0]
-            new_gp = np.array([gp[ind_0], gp[ind_1]])
-            new_gp = new_gp.reshape((1, new_gp.size, d[0].size))
+            ind_0, ind_1 = np.array(np.where(dist_rs == min_dist)).T[0]
+            new_gp = np.append(gp[ind_0], gp[ind_1])
 
-            total_gp.append(new_gp.tolist())
-            gp_index = np.delete(gp_index, [gp_index[ind_0], gp_index[ind_1]])
-            gp_index = np.append(gp_index, len(total_gp) - 1)
-
+            total_gp.append(new_gp.flatten().tolist())
             loop_rs.append(
-                [gp_index[ind_0], gp_index[ind_1], min_dist, new_gp.size])
+                [gp_ind[ind_0], gp_ind[ind_1], min_dist, new_gp.size])
 
-            a = 1
+            gp_ind = np.delete(gp_ind, [ind_0, ind_1])
+            gp_ind = np.append(gp_ind, len(total_gp) - 1)
+            gp_inds.append(gp_ind)
+
+        lb_v = np.linspace(0, k, k, endpoint=False).astype(np.int32)
+        lb_d = np.zeros((d.shape[0]), dtype=np.int32)
+        for i in range(k):
+            gp_slt = [np.where(d == j)[0][0] for j in total_gp[gp_inds[-k][i]]]
+            for j in gp_slt:
+                lb_d[j] = lb_v[i]
+
+        return lb_d, loop_rs
 
 
 test_arr = np.array([16.9, 38.5, 39.5, 80.8, 82, 34.6, 116.1])
-test_arr = test_arr.reshape(1, test_arr.shape[0], np.array(test_arr[0]).size)
-a = test_arr.tolist()
 
 
 def main():
     main_p = HierCluster()
-    main_p.fit(test_arr, 2)
+    label_s, loop_i = main_p.fit(test_arr, 2)
+    print(label_s)
+
+    plt.title("Hierarchical Clustering Dendrogram")
+    dendrogram(loop_i)
+    plt.xlabel(
+        "Number of points in node (or index of point if no parenthesis).")
+    plt.show()
+    plt.close()
 
 
 if __name__ == "__main__":
